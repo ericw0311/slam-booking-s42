0658d381e934c5f1c1b410977683c59535e6a286
Eric Willard <eric.willard@slam-data.net>
2019-05-19 05:59:42 +0200

    20190519_am1

--- src/Api/DateFormatFunctionDQL.php
+++ src/Api/DateFormatFunctionDQL.php
@@ -15,30 +15,30 @@ use Doctrine\ORM\Query\Parser;
 */
 class DateFormatFunctionDQL extends FunctionNode
 {
-	/**
+    /**
     * Holds the timestamp of the DATE_FORMAT DQL statement
     * @var $dateExpression
     */
     protected $dateExpression;
 
-	/**
-	* Holds the '% format' parameter of the DATE_FORMAT DQL statement
-	* var String
-	*/
-	protected $formatChar;
+    /**
+    * Holds the '% format' parameter of the DATE_FORMAT DQL statement
+    * var String
+    */
+    protected $formatChar;
 
-	public function getSql(SqlWalker $sqlWalker)
-	{
-	return 'DATE_FORMAT (' . $sqlWalker->walkArithmeticExpression( $this->dateExpression ) . ',' . $sqlWalker->walkStringPrimary( $this->formatChar ) . ')';
-	}
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return 'DATE_FORMAT (' . $sqlWalker->walkArithmeticExpression($this->dateExpression) . ',' . $sqlWalker->walkStringPrimary($this->formatChar) . ')';
+    }
 
-	public function parse(Parser $parser)
-	{
-	$parser->Match( Lexer::T_IDENTIFIER );
-	$parser->Match( Lexer::T_OPEN_PARENTHESIS );
-	$this->dateExpression = $parser->ArithmeticExpression();
-	$parser->Match( Lexer::T_COMMA );
-	$this->formatChar = $parser->ArithmeticExpression();
-	$parser->Match( Lexer::T_CLOSE_PARENTHESIS );
-	}
+    public function parse(Parser $parser)
+    {
+        $parser->Match(Lexer::T_IDENTIFIER);
+        $parser->Match(Lexer::T_OPEN_PARENTHESIS);
+        $this->dateExpression = $parser->ArithmeticExpression();
+        $parser->Match(Lexer::T_COMMA);
+        $this->formatChar = $parser->ArithmeticExpression();
+        $parser->Match(Lexer::T_CLOSE_PARENTHESIS);
+    }
 }
